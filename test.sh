7783792162a1f92adc32a0d5d7344e3f157dc79a
Charles Comstock <dgtized@gmail.com>
2018-04-20 16:43:12 -0500

    extract VERBOSE flag to make testing more silent

--- test.sh
+++ test.sh
@@ -2,6 +2,8 @@
 
 set -exuo pipefail
 
+VERBOSE="-v"
+
 rm -rf a_dir a_file scrapyard
 mkdir -p a_dir
 echo "content" > a_file
@@ -9,27 +11,27 @@ echo "content" > a_dir/a_file
 
 echo "** Search"
 
-./scrapyard.rb -v search -k "key-#(a_file)" -y scrapyard -p a_dir ||
+./scrapyard.rb $VERBOSE search -k "key-#(a_file)" -y scrapyard -p a_dir ||
     echo "SUCCESS"
 
 echo "** Dump/Search"
 
-./scrapyard.rb -v dump -k "key-#(a_file)" -y scrapyard -p a_dir &&
+./scrapyard.rb $VERBOSE dump -k "key-#(a_file)" -y scrapyard -p a_dir &&
     echo "SUCCESS"
 
-./scrapyard.rb -v search -k "key-#(a_file)" -y scrapyard -p a_dir &&
+./scrapyard.rb $VERBOSE search -k "key-#(a_file)" -y scrapyard -p a_dir &&
     echo "SUCCESS"
 
 echo "** Multi Key Dump/Junk/Search"
 
-./scrapyard.rb -v dump -k "key-#(a_file)","key-" -y scrapyard -p a_dir &&
+./scrapyard.rb $VERBOSE dump -k "key-#(a_file)","key-" -y scrapyard -p a_dir &&
     echo "SUCCESS"
 
-./scrapyard.rb -v junk -k "key-#(a_file)" -y scrapyard -p a_dir &&
+./scrapyard.rb $VERBOSE junk -k "key-#(a_file)" -y scrapyard -p a_dir &&
     echo "SUCCESS"
 
-./scrapyard.rb -v search -k "key-#(a_file)","key-" -y scrapyard -p a_dir &&
+./scrapyard.rb $VERBOSE search -k "key-#(a_file)","key-" -y scrapyard -p a_dir &&
     echo "SUCCESS"
 
-./scrapyard.rb -v crush -y scrapyard &&
+./scrapyard.rb $VERBOSE crush -y scrapyard &&
     echo "SUCCESS"
