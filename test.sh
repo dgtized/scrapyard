b55ecccf17a0a910334c554f8e74e690c29a6bf1
Charles Comstock <dgtized@gmail.com>
2018-04-20 16:31:47 -0500

    include multi-key search

--- test.sh
+++ test.sh
@@ -7,8 +7,12 @@ mkdir -p a_dir
 echo "content" > a_file
 echo "content" > a_dir/a_file
 
+echo "** Search"
+
 ./scrapyard.rb -v search -k "key-#(a_file)" -y scrapyard -p a_dir ||
     echo "SUCCESS"
+
+echo "** Dump/Search"
 
 ./scrapyard.rb -v dump -k "key-#(a_file)" -y scrapyard -p a_dir &&
     echo "SUCCESS"
@@ -16,9 +20,14 @@ echo "content" > a_dir/a_file
 ./scrapyard.rb -v search -k "key-#(a_file)" -y scrapyard -p a_dir &&
     echo "SUCCESS"
 
+echo "** Multi Key Dump/Junk/Search"
+
+./scrapyard.rb -v dump -k "key-#(a_file)","key-" -y scrapyard -p a_dir &&
+    echo "SUCCESS"
+
 ./scrapyard.rb -v junk -k "key-#(a_file)" -y scrapyard -p a_dir &&
     echo "SUCCESS"
 
-./scrapyard.rb -v search -k "key-#(a_file)" -y scrapyard -p a_dir ||
+./scrapyard.rb -v search -k "key-#(a_file)","key-" -y scrapyard -p a_dir &&
     echo "SUCCESS"
 
