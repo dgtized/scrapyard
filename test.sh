2940d4b0c0d776bb784065b487fc5ad7da34cb0a
Charles Comstock <dgtized@gmail.com>
2018-08-24 22:31:33 -0500

    parameterize the yard in test

--- test.sh
+++ test.sh
@@ -8,33 +8,34 @@ rm -rf a_dir a_file scrapyard
 mkdir -p a_dir
 echo "content" > a_file
 echo "content" > a_dir/a_file
+YARD=scrapyard
 
 echo "** Search"
 
-./scrapyard.rb $VERBOSE search -k "key-#(a_file)" -y scrapyard -p a_dir ||
+./scrapyard.rb $VERBOSE search -k "key-#(a_file)" -y $YARD -p a_dir ||
     echo "SUCCESS"
 
 echo "** Store/Search"
 
-./scrapyard.rb $VERBOSE store -k "key-#(a_file)" -y scrapyard -p a_dir &&
+./scrapyard.rb $VERBOSE store -k "key-#(a_file)" -y $YARD -p a_dir &&
     echo "SUCCESS"
 
-./scrapyard.rb $VERBOSE search -k "key-#(a_file)" -y scrapyard -p a_dir &&
+./scrapyard.rb $VERBOSE search -k "key-#(a_file)" -y $YARD -p a_dir &&
     echo "SUCCESS"
 
 echo "** Multi Key Store/Junk/Search"
 
-./scrapyard.rb $VERBOSE store -k "key-#(a_file)","key" -y scrapyard -p a_dir &&
+./scrapyard.rb $VERBOSE store -k "key-#(a_file)","key" -y $YARD -p a_dir &&
     echo "SUCCESS"
 
-./scrapyard.rb $VERBOSE store -k "key" -y scrapyard -p a_dir &&
+./scrapyard.rb $VERBOSE store -k "key" -y $YARD -p a_dir &&
     echo "SUCCESS"
 
-./scrapyard.rb $VERBOSE junk -k "key-#(a_file)" -y scrapyard -p a_dir &&
+./scrapyard.rb $VERBOSE junk -k "key-#(a_file)" -y $YARD -p a_dir &&
     echo "SUCCESS"
 
-./scrapyard.rb $VERBOSE search -k "key-#(a_file)","key","k" -y scrapyard -p a_dir &&
+./scrapyard.rb $VERBOSE search -k "key-#(a_file)","key","k" -y $YARD -p a_dir &&
     echo "SUCCESS"
 
-./scrapyard.rb $VERBOSE crush -y scrapyard &&
+./scrapyard.rb $VERBOSE crush -y $YARD &&
     echo "SUCCESS"
