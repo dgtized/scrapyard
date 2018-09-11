36f1d498af804ec186ebc6e8ba8f1c9b713fa0f9
Charles Comstock <dgtized@gmail.com>
2018-09-11 14:24:05 -0500

    Add assertions about multiple matches

--- spec/scrapyard_spec.rb
+++ spec/scrapyard_spec.rb
@@ -12,17 +12,22 @@ RSpec.describe "Commands" do
     system("rm -rf yard tmp")
   end
 
-  def make_cache(contents)
+  def make_cache(name, contents)
     Dir.mktmpdir(nil, "tmp") do |dir|
       IO.write("#{dir}/foo", contents)
-      expect(system("./scrapyard.rb store -k key -y yard -p #{dir}")).to be_truthy
+      expect(system("./scrapyard.rb store -k #{name} -y yard -p #{dir}")).to be_truthy
       dir
     end
   end
 
+  def assert_cache(name, contents)
+    expect(File.exist?(name)).to be_truthy
+    expect(IO.read(name + "/" + "foo")).to eq contents
+  end
+
   it 'creates a cache' do
     contents = "alpaca"
-    dir = make_cache(contents)
+    dir = make_cache("key", contents)
 
     expect(File.exist?(dir)).to be_falsey
 
@@ -30,8 +35,15 @@ RSpec.describe "Commands" do
 
     expect(system("./scrapyard.rb search -k key -y yard -p #{dir}")).to be_truthy
 
-    expect(File.exist?(dir)).to be_truthy
+    assert_cache(dir, contents)
+  end
 
-    expect(IO.read(dir + "/" + "foo")).to eq contents
+  it 'seaches by mtime for multiple caches' do
+    cacheA = make_cache("key-A", "a")
+    cacheB = make_cache("key-B", "b")
+
+    expect(system("./scrapyard.rb search -k key -y yard -p #{cacheB}")).to be_truthy
+
+    assert_cache(cacheB, "b")
   end
 end
