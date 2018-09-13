c82c133c751a0528246dc3c2f62d933203f96910
Charles Comstock <dgtized@gmail.com>
2018-09-13 18:20:54 -0500

    Lift Key into it's own file

--- lib/scrapyard.rb
+++ lib/scrapyard.rb
@@ -1,3 +1,5 @@
 module Scrapyard
   VERSION = '0.2.0'
+
+  require 'scrapyard/key'
 end
---
+++ lib/scrapyard/key.rb
@@ -0,0 +1,30 @@
+module Scrapyard
+  class Key
+    def initialize(key)
+      @key = key
+    end
+
+    def checksum!(log)
+      @key = @key.gsub(/(#\([^}]+\))/) do |match|
+        f = Pathname.new match[2..-2].strip
+        if f.exist?
+          log.debug "Including sha1 of #{f}"
+          Digest::SHA1.file(f).hexdigest
+        else
+          log.debug "File #{f} does not exist, ignoring checksum"
+          ''
+        end
+      end
+
+      self
+    end
+
+    def to_s
+      @key
+    end
+
+    def self.to_path(yard, keys, suffix, log)
+      keys.map { |k| yard.to_path + (Key.new(k).checksum!(log).to_s + suffix) }
+    end
+  end
+end
