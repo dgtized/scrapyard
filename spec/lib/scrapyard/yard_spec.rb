2da36495c28eb2a5a84396f6d580dc378a02e437
Charles Comstock <dgtized@gmail.com>
2018-09-18 15:31:03 -0500

    specs for match and most recent match

--- spec/lib/scrapyard/yard_spec.rb
+++ spec/lib/scrapyard/yard_spec.rb
@@ -12,5 +12,24 @@ RSpec.describe Scrapyard::Yard do
       s3.stub_responses(:list_objects, contents: [])
       expect(yard.search(["foo"])).to be_nil
     end
+
+    it "finds key if match" do
+      s3.stub_responses(
+        :list_objects, contents: [{key: 'key.tgz', last_modified: Time.now}]
+      )
+
+      expect(yard.search(["key"])).to eq(Pathname.new("/tmp/key.tgz"))
+    end
+
+    it "finds most recent key when matching multiple" do
+      s3.stub_responses(
+        :list_objects, contents: [
+          {key: 'key-old.tgz', last_modified: Time.now - 100},
+          {key: 'key-new.tgz', last_modified: Time.now}
+        ]
+      )
+
+      expect(yard.search(["key"])).to eq(Pathname.new("/tmp/key-new.tgz"))
+    end
   end
 end
