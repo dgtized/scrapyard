db8632bb01c8bde30a55373545b011f45f8a4c99
Charles Comstock <dgtized@gmail.com>
2018-09-18 15:59:50 -0500

    extract common value of now

--- spec/lib/scrapyard/yard_spec.rb
+++ spec/lib/scrapyard/yard_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe Scrapyard::Yard do
   let(:s3) { Aws::S3::Client.new(stub_responses: true) }
   let(:logger) { double(info: nil, debug: nil)}
   let(:yard) { Scrapyard::AwsS3Yard.new("", logger, client: s3) }
+  let(:now) { Time.now }
   context "search" do
     it "returns nil if bucket is empty" do
       s3.stub_responses(:list_objects, contents: [])
@@ -15,7 +16,7 @@ RSpec.describe Scrapyard::Yard do
 
     it "finds key if match" do
       s3.stub_responses(
-        :list_objects, contents: [{key: 'key.tgz', last_modified: Time.now}]
+        :list_objects, contents: [{key: 'key.tgz', last_modified: now}]
       )
 
       expect(yard.search(["key"])).to eq(Pathname.new("/tmp/key.tgz"))
@@ -25,8 +26,8 @@ RSpec.describe Scrapyard::Yard do
     it "finds most recent key when bucket contains multiple matches" do
       s3.stub_responses(
         :list_objects, contents: [
-          {key: 'key-old.tgz', last_modified: Time.now - 100},
-          {key: 'key-new.tgz', last_modified: Time.now}
+          {key: 'key-old.tgz', last_modified: now - 100},
+          {key: 'key-new.tgz', last_modified: now}
         ]
       )
 
@@ -37,13 +38,13 @@ RSpec.describe Scrapyard::Yard do
     it "finds first matching prefix when matching multiple" do
       s3.stub_responses(
         :list_objects, contents: [
-          {key: 'key-1.tgz', last_modified: Time.now},
-          {key: 'key-2.tgz', last_modified: Time.now}
+          {key: 'key-1.tgz', last_modified: now},
+          {key: 'key-2.tgz', last_modified: now}
         ]
       )
 
       expect(yard.search(["key"])).to eq(Pathname.new("/tmp/key-1.tgz"))
-      expect(yard.search(["foo", "bar"])).to be_nil
+      expect(yard.search(%w[foo bar])).to be_nil
       expect(yard.search(["key-2", "key"])).to eq(Pathname.new("/tmp/key-2.tgz"))
     end
   end
