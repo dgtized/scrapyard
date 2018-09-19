47a638a2a73d6d98afa433ebc136940b5d4bc259
Charles Comstock <dgtized@gmail.com>
2018-09-19 02:40:02 -0500

    Assert that Junk removes a key

--- spec/lib/scrapyard/runner_spec.rb
+++ spec/lib/scrapyard/runner_spec.rb
@@ -3,13 +3,24 @@ require 'scrapyard'
 require 'fileutils'
 
 RSpec.describe Scrapyard::Runner do
-  before(:all) { FileUtils.rmtree('scrapyard') }
+  before(:all) do
+    FileUtils.rmtree('scrapyard')
+    FileUtils.mkdir_p('scrapyard')
+  end
   let(:log) { spy('log') }
   let(:runner) { Scrapyard::Runner.new("scrapyard", log, {})}
   context "#store" do
     it "creates a tarball" do
       expect { runner.store(["key"], ['scrapyard.gemspec']) }.
         to change { File.exist?('scrapyard/key.tgz') }.to(true)
+    end
+  end
+
+  context "#junk" do
+    it "removes tarball from cache" do
+      FileUtils.touch 'scrapyard/key.tgz'
+      expect { runner.junk(["key"], []) }.
+        to change { File.exist?('scrapyard/key.tgz') }.from(true)
     end
   end
 end
